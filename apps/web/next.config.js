712005240cbb872d13686b06a697068d3c716f11
Egor88888888 <dyakovegor@gmail.com>
2025-05-04 18:12:54 +0300

    Enable @-alias in Next.js and fix Netlify config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['db.supabase.co'],
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 }
 
